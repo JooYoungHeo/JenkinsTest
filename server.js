bfcd3492b928c69aa8e1ee652043fd36f0ed0708
JooYoungHeo <jyhur900226@naver.com>
2019-03-03 23:07:14 +0900

    [Work] test'

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'application/json'});
-	let json = JSON.stringify({test: 'jenkins simple'});
+	let json = JSON.stringify({test: 'jenkins'});
 	res.end(json);
 }).listen(9000);
